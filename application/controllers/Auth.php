f3435c25595ccb2cb465fd55d7ba7e40ec3a8ba5
Dirga <dirgantoro.developer@gmail.com>
2020-05-18 22:16:37 +0700

    Add adjustment
    Add adjustment

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,11 +1,12 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 require APPPATH . '/controllers/Core.php';
 
 class Auth extends Core
-{    
-    public function __construct(){
+{
+    public function __construct()
+    {
 
         parent::__construct();
         $this->load->model('User');
@@ -15,20 +16,21 @@ class Auth extends Core
     {
         // $this->res->setToken(AUTHORIZATION::generateToken($this->req));
         $this->set_response(
-            $this->res->SetObject(RCD::SC, RCD::SD, NULL, AUTHORIZATION::generateToken($this->req)), Rest::HTTP_OK);
+            $this->res->SetObject(RCD::SC, RCD::SD, NULL, AUTHORIZATION::generateToken($this->req)),
+            Rest::HTTP_OK
+        );
     }
 
     public function register_post()
     {
-        $this->req->password = password_hash($this->req->password, PASSWORD_DEFAULT);        
-        $this->set_response( $this->User->add_user($this->req) , Rest::HTTP_OK);
+        $this->req->password = password_hash($this->req->password, PASSWORD_DEFAULT);
+        $this->set_response($this->User->add_user($this->req), Rest::HTTP_OK);
     }
-    
+
     public function forgotpassword_post()
     {
         $res =  $this->User->add_forgotpassword($this->req);
-        if($res->status == RCD::EC_INSERT)
-        {
+        if ($res->status == RCD::EC_INSERT) {
             $this->res->SetStatus(RCD::EC_INSERT, RCD::ED_INSERT);
         }
         return $this->req;
@@ -38,5 +40,4 @@ class Auth extends Core
     {
         $this->set_response($this->res, Rest::HTTP_OK);
     }
-    
 }
--- application/controllers/Core.php
+++ application/controllers/Core.php
@@ -1,78 +1,76 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 require APPPATH . '/libraries/Rest.php';
 require APPPATH . '/libraries/Response.php';
 
-class Core extends Rest{
+class Core extends Rest
+{
      /**
-     * Request object. 
-     * 
-     * @var Request
-     */
+      * Request object. 
+      * 
+      * @var Request
+      */
      public $req;
      /**
-     * Response object. 
-     * 
-     * @var Response
-     */
+      * Response object. 
+      * 
+      * @var 
+      */
      public $res;
-      /**
-     * Skiped Url. 
-     * 
-     * @var Response
-     */
+     /**
+      * Skiped Url. 
+      * 
+      * @var 
+      */
      public $skipedUrl = array(
           'auth/register',
           'auth/login'
      );
      /**
-     * __construct. 
-     * 
-     * @var __construct
-     */
-     function __construct() {     
-            
+      * __construct. 
+      * 
+      * @var __construct
+      */
+     function __construct()
+     {
+
           parent::__construct();
 
           $this->Initiate();
      }
      /**
-     * Initiate. 
-     * 
-     * @var Initiate
-     */
+      * Initiate. 
+      * 
+      * @var Initiate
+      */
      function Initiate()
      {
           $this->res = new Response();
 
           $this->req =  json_decode($this->request->body[0]);
 
-          if(!in_array(uri_string(), $this->skipedUrl))
-          {
+          if (!in_array(uri_string(), $this->skipedUrl)) {
                $headers = $this->input->request_headers();
 
-               if (array_key_exists('Authorization', $headers) && !empty($headers['Authorization']))
-               {
+               if (array_key_exists('Authorization', $headers) && !empty($headers['Authorization'])) {
                     $decodedToken = AUTHORIZATION::validateToken($headers['Authorization']);
-                    if ($decodedToken != false) 
-                    {
+                    if ($decodedToken != false) {
                          $this->res->SetData($decodedToken);
-                    }
-                    else
-                    {
+                    } else {
                          // $this->res->SetStatus(RCD::EC_UNAUTH, RCD::ED_UNAUTH);
                          $this->set_response(
-                              $this->res->SetObject(RCD::EC_UNAUTH, RCD::ED_UNAUTH, FALSE, null), Rest::HTTP_UNAUTHORIZED);
+                              $this->res->SetObject(RCD::EC_UNAUTH, RCD::ED_UNAUTH, FALSE, null),
+                              Rest::HTTP_UNAUTHORIZED
+                         );
                     }
-               }
-               else
-               {
+               } else {
                     // $this->res->SetStatus(RCD::EC_UNAUTH, RCD::ED_UNAUTH);
                     $this->set_response(
-                         $this->res->SetObject(RCD::EC_UNAUTH, RCD::ED_UNAUTH, FALSE), Rest::HTTP_UNAUTHORIZED);
+                         $this->res->SetObject(RCD::EC_UNAUTH, RCD::ED_UNAUTH, FALSE),
+                         Rest::HTTP_UNAUTHORIZED
+                    );
                }
-
           }
      }
 }
